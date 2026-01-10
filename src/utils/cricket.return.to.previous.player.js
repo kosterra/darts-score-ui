f16ec80614dc2284f7ee274077ee78d83e16c319
kosterra <rkoster@gmx.ch>
2026-01-10 17:15:23 +0100

    Bugfixes

--- src/utils/cricket.return.to.previous.player.js
+++ src/utils/cricket.return.to.previous.player.js
@@ -1,55 +1,66 @@
-import { changeCurrentPlayer, cricketGetCurrentScore } from './game.utils';
+import {
+    changeCurrentPlayer,
+    cricketGetCurrentScore
+} from './game.utils';
 
 const CricketReturnToPreviousPlayer = game => {
-    const prevThrowInfo = game.allThrows.pop();
+    let prevThrowInfo = game.allThrows.pop();
 
     changeCurrentPlayer(game);
+
     game.currentThrow = prevThrowInfo.darts;
 
-    updateTotalThrow(game, prevThrowInfo.playerId, prevThrowInfo.darts.length);
+    updateTotalThrow(
+        game,
+        prevThrowInfo.playerId,
+        prevThrowInfo.darts.length
+    );
+
     updateSectionHit(game.playerModels[prevThrowInfo.playerId], prevThrowInfo.darts);
     updateHit(game.playerModels[prevThrowInfo.playerId], prevThrowInfo.darts);
 
-    const prevScore = cricketGetCurrentScore(
-        game.currentThrow,
-        game.playerModels[prevThrowInfo.playerId],
-        game.playerModels[prevThrowInfo.playerId].sectionHit
-    );
+    let prevScore = cricketGetCurrentScore(game.currentThrow, game.playerModels[prevThrowInfo.playerId], game.playerModels[prevThrowInfo.playerId].sectionHit);
     game.playerModels[prevThrowInfo.playerId].score = prevScore;
 
     return game;
-};
+}
 
 const updateTotalThrow = (game, playerId, dartsNbr) => {
-    const totalThrow = game.playerModels[playerId].totalThrow;
+    let totalThrow = game.playerModels[playerId].totalThrow;
     totalThrow.game.rounds--;
     totalThrow.game.darts -= dartsNbr;
     game.playerModels[playerId].totalThrow = totalThrow;
-};
+}
 
 const updateSectionHit = (playerModel, darts) => {
     darts.forEach(dart => {
-        const section = Number(dart.slice(1));
-        if (section < 15) return;
+        let section = Number(dart.slice(1));
 
-        const hits = { t: 3, d: 2, s: 1 };
-        const type = dart[0].toLowerCase();
+        if (section >= 15) {
 
-        if (hits[type]) {
-            playerModel.sectionHit[section] = Math.max(
-                0,
-                playerModel.sectionHit[section] - hits[type]
-            );
+            if (/t/i.test(dart[0])) {
+                playerModel.sectionHit[section] >= 3 ?
+                    playerModel.sectionHit[section] = playerModel.sectionHit[section] - 3 : playerModel.sectionHit[section] = 0;
+            }
+
+            if (/d/i.test(dart[0])) {
+                playerModel.sectionHit[section] >= 2 ?
+                    playerModel.sectionHit[section] = playerModel.sectionHit[section] - 2 : playerModel.sectionHit[section] = 0;
+            }
+
+            if (/s/i.test(dart[0])) {
+                playerModel.sectionHit[section] > 0 && playerModel.sectionHit[section]--;
+            }
         }
-    });
-};
+    })
+}
 
 const updateHit = (playerModel, darts) => {
     darts.forEach(dart => {
         if (Number(dart) === 0) dart = 'Missed';
-        if (playerModel.hit[dart] > 0) playerModel.hit[dart]--;
-        if (playerModel.hit[dart] === 0) delete playerModel.hit[dart];
-    });
-};
+        playerModel.hit[dart] > 0 && playerModel.hit[dart]--;
+        playerModel.hit[dart] === 0 && delete playerModel.hit[dart];
+    })
+}
 
 export default CricketReturnToPreviousPlayer;
